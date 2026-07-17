21a7a703a6e401738c9882fa9831bcfd58a2717f
taha-javed-dev <taha.javed.dev@gmail.com>
2026-07-17 16:19:43 +0530

    feat: add connection-service

---
+++ connection-service/src/main/java/com/taha/linkedin/connection_service/service/ConnectionsService.java
@@ -0,0 +1,23 @@
+package com.taha.linkedin.connection_service.service;
+
+
+import com.taha.linkedin.connection_service.entity.Person;
+import com.taha.linkedin.connection_service.repository.PersonRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ConnectionsService {
+
+    private final PersonRepository personRepository;
+
+    public List<Person> getFirstDegreeConnections(Long userId) {
+        log.info("Getting first degree connections for user with id: {}", userId);
+        return personRepository.getFirstDegreeConnections(userId);
+    }
+}
